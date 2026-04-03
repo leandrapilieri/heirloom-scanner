9a1d7cb7a3216172bce8c5f3831db38e1ef9faab
leandrapilieri <leandrapiliery@gmail.com>
2026-04-03 17:14:59 -0400

    Remove conflicting id API routes to unblock build

--- app/api/retailers/[id]/route.ts
+++
@@ -1,7 +0,0 @@
-import { NextResponse } from "next/server";
-import { getRetailersById } from "@/lib/retailers";
-
-export async function GET(_: Request, { params }: { params: Promise<{ id: string }> }) {
-  const { id } = await params;
-  return NextResponse.json({ productId: id, retailers: getRetailersById(id), mocked: true });
-}
